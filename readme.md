bd7f9b0d09bb27128b97e00c9f3cbeef92c6429f
Emanuel Henrique <emanuelvieiraDev@gmail.com>
2022-09-06 10:54:52 -0300

    project finish

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Desafio corrigindo código
+
+Nesse desafio havia uma aplicação Nodejs que estava em processo de desenvolvimento, porém, alguns testes nao estavam passando, dessa forma o objetivo do desafio era corrigir e refatorar o código a fim de que todos os testes fossem aprovados. Veja a descrição completa aqui ➡ [Desafio-corrigindo-codigo](https://www.notion.so/Desafio-03-Corrigindo-o-c-digo-c15c8a2e212846039a367cc7b763c6dd#93a7a75c996e42cea90b6a114a78e3b5)
+
+## Tecnologias
+- Javascript
+- Nodejs
+- express
+- yarn
+- Nodemon
+- uuid
+
+## Como executar 
+
+        1 - Clone o repositório
+        2 - yarn (para instalar as dependencias)
+        3 - yarn test (Para ver os testes que foram aprovados) ou yarn dev (Para executar o projeto)
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
@@ -22,37 +22,40 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
+  
+  repositories.push(repository);
 
-  return response.json(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const findRepository = repositories.find(repo => repo.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+
+  if(!findRepository){
+    return response.status(404).json({ error: "Repository not found."})
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  findRepository.title = title ? title: findRepository.title;
+  findRepository.url = url ? url : findRepository.url;
+  findRepository.techs = techs ? techs : findRepository.techs;
 
-  repositories[repositoryIndex] = repository;
-
-  return response.json(repository);
+  return response.json(findRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  const findRepositoryIndex = repositories.findIndex(repo => repo.id === id);
+  
+  if(findRepositoryIndex === -1){
+    return response.status(404).json({ error: 'Repository not found. '})
   }
 
-  repositories.splice(repositoryIndex, 1);
+  repositories.splice(findRepositoryIndex, 1);
 
   return response.status(204).send();
 });
@@ -60,15 +63,15 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const findRepository = repositories.find(repo => repo.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  if(!findRepository){
+    return response.status(404).json({ error: 'Repository not found. '})
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  findRepository.likes++;
 
-  return response.json('likes');
+  return response.json(findRepository);
 });
 
 module.exports = app;
